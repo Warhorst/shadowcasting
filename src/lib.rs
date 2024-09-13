c2065fa0a8fa535f811d860b05187968e15d216c
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-09-13 07:29:07 +0200

    The shadowcasting now uses pad

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use std::collections::HashSet;
+use pad::{p, Position};
 use Octant::*;
 
 pub struct ShadowCasting<'a> {
     /// The position from where the line of sight originates
-    origin: (isize, isize),
-    /// Closure which tells if if a given position blocks the view
-    position_blocks_view: Box<dyn Fn((isize, isize)) -> bool + 'a>,
+    origin: Position,
+    /// Closure which tells if a given position blocks the view
+    position_blocks_view: Box<dyn Fn(Position) -> bool + 'a>,
     /// The set which holds all visible positions. Initially empty
-    visible_positions: HashSet<(isize, isize)>,
+    visible_positions: HashSet<Position>,
     /// The maximum distance of the line of sight from the origin.
     /// Everything beyond this distance will not be visible.
     max_distance: usize,
@@ -15,8 +16,8 @@ pub struct ShadowCasting<'a> {
 
 impl<'a> ShadowCasting<'a> {
     pub fn new(
-        origin: (isize, isize),
-        position_blocks_view: impl Fn((isize, isize)) -> bool + 'a,
+        origin: Position,
+        position_blocks_view: impl Fn(Position) -> bool + 'a,
         max_distance: usize,
     ) -> Self {
         ShadowCasting {
@@ -29,7 +30,7 @@ impl<'a> ShadowCasting<'a> {
 
     /// Calculate the line of sight in all directions and relative to origin with recursive shadow casting.
     /// Returns a set with all visible positions.
-    pub fn calculate_los(mut self) -> HashSet<(isize, isize)> {
+    pub fn calculate_los(mut self) -> HashSet<Position> {
         self.set_pos_visible(self.origin);
 
         for octant in Octant::octants() {
@@ -53,7 +54,7 @@ impl<'a> ShadowCasting<'a> {
         let mut prev_pos_blocks_view = false;
 
         for i in (0..=depth).rev() {
-            let template_pos = (i as isize, depth as isize);
+            let template_pos = p!(i, depth);
             let pos = octant.get_world_coordinate(self.origin, i, depth);
             let left_slope = Self::calculate_left_slope(template_pos);
             let right_slope = Self::calculate_right_slope(template_pos);
@@ -89,19 +90,19 @@ impl<'a> ShadowCasting<'a> {
         }
     }
 
-    fn calculate_left_slope((x, y): (isize, isize)) -> f32 {
-        (x as f32 + 0.5) / (y as f32 - 0.5)
+    fn calculate_left_slope(pos: Position) -> f32 {
+        (pos.x as f32 + 0.5) / (pos.y as f32 - 0.5)
     }
 
-    fn calculate_right_slope((x, y): (isize, isize)) -> f32 {
-        (x as f32 - 0.5) / (y as f32 + 0.5)
+    fn calculate_right_slope(pos: Position) -> f32 {
+        (pos.x as f32 - 0.5) / (pos.y as f32 + 0.5)
     }
 
-    fn pos_blocks_view(&self, pos: (isize, isize)) -> bool {
+    fn pos_blocks_view(&self, pos: Position) -> bool {
         (self.position_blocks_view)(pos)
     }
 
-    fn set_pos_visible(&mut self, pos: (isize, isize)) {
+    fn set_pos_visible(&mut self, pos: Position) {
         self.visible_positions.insert(pos);
     }
 }
@@ -155,15 +156,15 @@ impl Octant {
     /// Transform the given depth and index to world coordinates relative to origin.
     fn get_world_coordinate(
         &self,
-        origin: (isize, isize),
+        origin: Position,
         i: usize,
         depth: usize,
-    ) -> (isize, isize) {
+    ) -> Position {
         let (xx, xy, yx, yy) = self.get_diffs();
 
-        (
-            origin.0 + i as isize * xx + depth as isize * xy,
-            origin.1 + i as isize * yx + depth as isize * yy
+        p!(
+            origin.x + i as isize * xx + depth as isize * xy,
+            origin.y + i as isize * yx + depth as isize * yy
         )
     }
 
@@ -178,5 +179,13 @@ impl Octant {
             LeftBottom => (0, -1, -1, 0),
             LeftTop => (0, -1, 1, 0),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn works() {
+
     }
 }
